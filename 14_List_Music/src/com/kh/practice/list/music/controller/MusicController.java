bf7a1d397b4aeb63d3a77f890d85697f472ea5de
Solbeen0531 <netps0201@gmail.com>
2023-06-07 16:55:21 +0900

    decorator

---
+++ 14_List_Music/src/com/kh/practice/list/music/controller/MusicController.java
@@ -0,0 +1,57 @@
+package com.kh.practice.list.music.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kh.practice.list.music.model.vo.Music;
+
+public class MusicController {
+	private List<Music> list = new ArrayList<Music>();
+
+	public int addList(Music music) {
+		int result = 0; // 0 추가실패,1추가성공
+		
+		// TODO
+
+		return result;
+	}
+
+	public int addAtZero(Music music) {
+		int result = 0;// 0 추가실패,1추가성공
+		// TODO
+
+		return result;
+	}
+
+	public List<Music> printAll() {
+		int result = 0;
+		// TODO
+		return list;
+
+	}
+
+	public Music SearchMusic(String title) {
+		Music result = null;
+		// TODO
+		return result;
+	}
+	
+	public Music removeMusic(String title) {
+		//TODO
+		return null;
+	}
+	
+	public Music setMusic(String title,Music music) {
+		//TODO
+		return null;
+	}
+	
+	public int ascTitle() {
+		int result = 0;
+		// TODO
+		return result;
+	}
+	
+	
+
+}
---
+++ 14_List_Music/src/com/kh/practice/list/music/model/vo/Music.java
@@ -0,0 +1,66 @@
+package com.kh.practice.list.music.model.vo;
+
+import java.util.Objects;
+
+public class Music {
+	private String title;
+	private String singer;
+	
+	public Music() {
+		
+	}
+	
+	public Music(String title, String singer) {
+		this.title = title;
+		this.singer = singer;
+	}
+	
+	public String getTitle() {
+		return singer;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	@Override
+	public String toString() {
+		return "Music [title=" + title + ", singer=" + singer + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(singer, title);
+		// return super.hashCode(singer,title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Music other = (Music) obj;
+		return Objects.equals(singer, other.singer) && Objects.equals(title, other.title);
+	}
+	
+	public int compareTo(Object o) {
+		int result = 0;
+		//TODO
+		
+		return result;
+		
+	}
+	
+	
+}
