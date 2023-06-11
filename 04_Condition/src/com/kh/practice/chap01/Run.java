a70137417f74fe2759c57862328f24002045abc7
Solbeen0531 <netps0201@gmail.com>
2023-06-11 18:53:20 +0900

    add condition2

--- 04_Condition/src/com/kh/practice/chap01/Run.java
+++ 04_Condition/src/com/kh/practice/chap01/Run.java
@@ -4,8 +4,15 @@ public class Run {
 
 	public static void main(String[] args) {
 
+		/* 문제 1
 		new ControlPractice().practice1(); // ControlPractice()에 정의된 practice1()을 실행한다
-
+		*/
+		
+		/* 문제 2
+		new ControlPractice().practice2(); // ControlPractice()에 정의된 practice2()을 실행한다 
+		*/
+		
+		
 	}
 
 }
