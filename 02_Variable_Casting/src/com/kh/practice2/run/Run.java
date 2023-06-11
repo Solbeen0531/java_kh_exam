72f84f559a9b3c2272550b37df53444d57eb5a27
Solbeen0531 <netps0201@gmail.com>
2023-06-11 13:39:10 +0900

    add casting

---
+++ 02_Variable_Casting/src/com/kh/practice2/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.practice2.run;
+
+import com.kh.practice2.func.CastingPractice1;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		
+		new CastingPractice1().method1(); // CastingPractice1에 있는 method1()을 실행
+		
+	}
+}
+
