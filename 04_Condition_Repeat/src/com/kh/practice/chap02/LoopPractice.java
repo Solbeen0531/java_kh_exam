b5b4150f75c22f641e83931bbb32964f8a1ea7e0
Solbeen0531 <netps0201@gmail.com>
2023-06-14 02:11:15 +0900

    add hard repeat frame

---
+++ 04_Condition_Repeat/src/com/kh/practice/chap02/LoopPractice.java
@@ -0,0 +1,22 @@
+package com.kh.practice.chap02;
+
+import java.util.Scanner;
+
+public class LoopPractice {
+	
+	// 스캐너 시작
+	Scanner sc = new Scanner(System.in);
+	
+	public void practice015() {
+		
+		
+		
+		
+		
+		
+		// 스캐너 끝
+		sc.close();
+	}
+	
+
+}
