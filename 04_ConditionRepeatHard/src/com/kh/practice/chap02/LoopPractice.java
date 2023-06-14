1eaa646964ede6fe908f22146a54fce059ba29c6
Solbeen0531 <netps0201@gmail.com>
2023-06-14 17:56:59 +0900

    66

--- 04_ConditionRepeatHard/src/com/kh/practice/chap02/LoopPractice.java
+++ 04_ConditionRepeatHard/src/com/kh/practice/chap02/LoopPractice.java
@@ -3,20 +3,23 @@ package com.kh.practice.chap02;
 import java.util.Scanner;
 
 public class LoopPractice {
-	
+
 	// 스캐너 시작
 	Scanner sc = new Scanner(System.in);
-	
+
 	public void practice015() {
-		
-		
-		
-		
-		
-		
-		// 스캐너 끝
-		sc.close();
+
+		System.out.print("숫자 : ");
+
+		// 정수형 숫자 입력
+		int num = sc.nextInt();
+
+		for (;;) {
+			for (int i = 1; i <= num; i++) {
+				System.out.print(i + "#");
+			}
+		}
+
 	}
-	
 
 }
--- 04_ConditionRepeatHard/src/com/kh/practice/chap02/Run.java
+++ 04_ConditionRepeatHard/src/com/kh/practice/chap02/Run.java
@@ -4,6 +4,7 @@ public class Run {
 
 	public static void main(String[] args) {
 		
+		// 실습 문제 15
 		// LoopPractice에 정의된 메서드, practice015 실행
 		new LoopPractice().practice015();
 	}
