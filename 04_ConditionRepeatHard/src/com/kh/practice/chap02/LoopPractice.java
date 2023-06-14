0a3cb6f7c1f848b96e8fa1c85d58edd56513a13b
Solbeen0531 <netps0201@gmail.com>
2023-06-14 22:52:28 +0900

    add

--- 04_ConditionRepeatHard/src/com/kh/practice/chap02/LoopPractice.java
+++ 04_ConditionRepeatHard/src/com/kh/practice/chap02/LoopPractice.java
@@ -14,12 +14,30 @@ public class LoopPractice {
 		// 정수형 숫자 입력
 		int num = sc.nextInt();
 
-		for (;;) {
-			for (int i = 1; i <= num; i++) {
-				System.out.print(i + "#");
+		// 입력한 값이 2보다 작으면
+		if (num < 2) {
+			System.out.println("잘못 입력하셨습니다.");
+
+			// 입력한 값이 (첫 번째 소수인) 2라면
+		} else if (num == 2) {
+			System.out.println("소수 입니다.");
+			
+			// 입력한 값이 2보다 크면
+		} else {
+
+			for (int i = 2; i <	num; i++) {
+
+				if (num % i == 0) {
+					System.out.println("소수가 아닙니다.");
+					break;
+				} else {
+				}
+				System.out.println("소수 입니다.");
+				break;
+
 			}
+
 		}
 
 	}
-
 }
