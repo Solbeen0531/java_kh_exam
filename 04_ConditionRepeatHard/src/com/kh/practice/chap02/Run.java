d594dc03002c32c206872f93169f195e95a2e3ff
Solbeen0531 <netps0201@gmail.com>
2023-06-15 12:48:41 +0900

    add

--- 04_ConditionRepeatHard/src/com/kh/practice/chap02/Run.java
+++ 04_ConditionRepeatHard/src/com/kh/practice/chap02/Run.java
@@ -5,12 +5,16 @@ public class Run {
 	public static void main(String[] args) {
 
 		// 실습 문제 15
-				// LoopPractice에 정의된 메서드, practice015 실행
-				// new LoopPractice().practice015();
+			// LoopPractice에 정의된 메서드, practice015 실행
+			// new LoopPractice().practice015();
 		
 		// 실습 문제 16
-				// LoopPractice에 정의된 메서드, practice016 실행
-				 new LoopPractice().practice016();
+			// LoopPractice에 정의된 메서드, practice016 실행
+			// new LoopPractice().practice016();
+		
+		// 실습 문제 17
+			// LoopPractice에 정의된 메서드, practice017 실행
+			 new LoopPractice().practice017();
 	}
 
 }
